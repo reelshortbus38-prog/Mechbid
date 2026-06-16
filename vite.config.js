020b46ef0e058bc53d5404eb5dd8073466660ae9
reelshortbus38-prog <reelshortbus38@gmail.com>
2026-06-16 15:04:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react({
-    include: '**/*.{jsx,js}',
-  })],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      }
-    }
-  }
+  plugins: [react()],
 });
